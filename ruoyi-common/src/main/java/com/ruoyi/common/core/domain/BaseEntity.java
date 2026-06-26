481d4360b7a5689127c60821ec3f568f299680e2
kyesheu <jaykai5991@gmail.com>
2026-06-26 11:09:23 +0800

    feat: add @Schema to BaseEntity for all subclass descriptions

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * Entity基类
@@ -22,23 +23,29 @@ public class BaseEntity implements Serializable
     private String searchValue;
 
     /** 创建者 */
+    @Schema(description = "创建者")
     private String createBy;
 
     /** 创建时间 */
+    @Schema(description = "创建时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /** 更新者 */
+    @Schema(description = "更新者")
     private String updateBy;
 
     /** 更新时间 */
+    @Schema(description = "更新时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     /** 备注 */
+    @Schema(description = "备注")
     private String remark;
 
     /** 请求参数 */
+    @Schema(description = "请求参数")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Map<String, Object> params;
 

ddaf486ee0bd04c64976f61b9d4ab807255d6a38
kye sheu <jaykai5991@gmail.com>
2026-06-29 22:08:13 +0800

    refactor: dynamically read server port and context-path in startup banner

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -3,10 +3,12 @@ package com.ruoyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
@@ -15,12 +17,19 @@ public class RuoYiApplication
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RuoYiApplication.class, args);
+        Environment env = context.getEnvironment();
+
+        String port = env.getProperty("local.server.port",
+                env.getProperty("server.port", "8080"));
+        String contextPath = env.getProperty("server.servlet.context-path", "");
+        String baseUrl = "http://localhost:" + port + contextPath;
+
         System.out.println("\n========================================");
         System.out.println("  Ticket Backend 启动成功");
-        System.out.println("  应用地址:  http://localhost:8080");
-        System.out.println("  Swagger:   http://localhost:8080/swagger-ui.html");
-        System.out.println("  Druid:     http://localhost:8080/druid");
+        System.out.println("  应用地址:  " + baseUrl);
+        System.out.println("  Swagger:   " + baseUrl + "/swagger-ui.html");
+        System.out.println("  Druid:     " + baseUrl + "/druid");
         System.out.println("========================================\n");
     }
 }
